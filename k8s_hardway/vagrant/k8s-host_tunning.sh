3e013a46aecd621a9029dc925de158f17ae73018
Admin <awuyong@aliyun.com>
2022-04-11 18:31:49 +0800

    k8s-host_tunning config added

---
+++ k8s_hardway/vagrant/k8s-host_tunning.sh
@@ -0,0 +1,23 @@
+setenforce 0
+sed -i '/SELINUX/s/enforcing/disabled/' /etc/selinux/config
+# 关闭防火墙
+systemctl stop firewalld && systemctl disable firewalld
+
+# 设置iptables规则
+iptables -F && iptables -X && iptables -F -t nat && iptables -X -t nat && iptables -P FORWARD ACCEPT
+# 关闭swap
+swapoff -a && free –h
+
+# 关闭dnsmasq(否则可能导致容器无法解析域名)
+service dnsmasq stop && systemctl disable dnsmasq
+
+cat > /etc/sysctl.d/kubernetes.conf <<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_nonlocal_bind = 1
+net.ipv4.ip_forward = 1
+vm.swappiness = 0
+vm.overcommit_memory = 1
+EOF
+# 生效文件
+sysctl -p /etc/sysctl.d/kubernetes.conf
